bb7f8fcf5bf954069b70e90b7a98419268843a30
Rahul Rai <rrai06125@gmail.com>
2023-07-08 12:14:04 +0530

    URL Changed in Socket config

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_BASE_URL,
     methods: ["GET", "POST"],
   },
 });
